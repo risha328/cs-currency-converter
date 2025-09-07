a0f3236e7757dfebf1c85938c0ffabd064678ac8
Risha Mondal <rishamondal328@gmail.com>
2025-09-07 20:07:27 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import Freecurrencyapi from '@everapi/freecurrencyapi-js';
+
+const freecurrencyapi = new Freecurrencyapi('fca_live_HTpF4hWCT1fzTBUTpd8Uu1aKr2ccuBnhlGgujXZk');
+
+//convertCurrency("USD", "INR", 50);
+export async function convertCurrency(fromCurrency, toCurrency, amount) {
+    const res = await freecurrencyapi.latest({
+        base_currency: fromCurrency,
+        currencies: toCurrency
+    });
+    const multiplier = res.data[toCurrency];
+    return multiplier * amount;
+}
+// freecurrencyapi.latest({
+//         base_currency: 'USD',
+//         currencies: 'INR'
+//     }).then(response => {
+//         console.log(response);
+//     });
